37c45c22ae48613e510cdf33a8c64da568dfa063
karvindass <karvin9@gmail.com>
2016-05-24 14:00:58 +0700

    Cleaned Up
    Code cleaned up a bit, reveal() function created

--- friendly-drink.py
+++ friendly-drink.py
@@ -1,18 +1,22 @@
-import time
+import time # Used for time delay function
 
-inputName = ""
+inputName = "" # Stores the name given to the program
 
+# Used to reveal text with the time delay
+def reveal(text):
+    print "FD: " + text
+    time.sleep(1)
+
+# Initial lines when started
 def start():
-    print "Hey there, my name is Friendly-Drink, but I go by FD."
-    time.sleep(1)
-    print "But I go by FD"
-    time.sleep(1)
+    reveal("Hey there, my name is Friendly-Drink, but I go by FD.")
+    reveal("But I go by FD")
 
+# Gets name of user
 def getName():
-    print "What's your name?"
+    reveal("What's your name?")
     inputName = raw_input('You: ')
-    print()
-    print "Hey there %s, it's nice to meet you" %inputName
+    reveal("Hey there %s, it's nice to meet you" %inputName)
 
 start()
 getName()
