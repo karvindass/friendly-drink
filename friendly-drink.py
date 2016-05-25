2a932e5400e7e97eccddeea0a6382a5978479330
karvindass <karvin9@gmail.com>
2016-05-25 16:58:07 +0700

    Main function changed
    Implement while loop type approach

--- friendly-drink.py
+++ friendly-drink.py
@@ -8,9 +8,10 @@ def reveal(text):
     time.sleep(1)
 
 # Initial lines when started
-def start():
+def intro():
     reveal("Hey there, my name is Friendly-Drink, but I go by FD.")
     reveal("But I go by FD")
+    getName()
 
 # Gets name of user
 def getName():
@@ -18,5 +19,12 @@ def getName():
     inputName = raw_input('You: ')
     reveal("Hey there %s, it's nice to meet you" %inputName)
 
+def start():
+    sTime = time.time() # time from program start
+    intro()
+    # while True:
+    qString = raw_input('> ') # Query string
+    if qString.startswith("Time in"):
+        getTime(qString[7:])
+
 start()
-getName()
