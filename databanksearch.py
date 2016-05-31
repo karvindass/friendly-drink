a0a58b02d40adb929ab909f34c46aa90ea6547d2
karvindass <karvin9@gmail.com>
2016-05-31 21:03:36 +0700

    Generate Address from Resource name

--- databanksearch.py
+++ databanksearch.py
@@ -6,3 +6,15 @@ from nltk.stem import WordNetLemmatizer # Used to lemmatize (find root word)
 
     for s,p,o in g:
         print s,p,o
+# Gen Resource string
+# Should find the link to the object of the interested thing
+# e.g. Kanye
+def getResource(resName):
+    resName = resName.title()
+    tokens = nltk.word_tokenize(resName)
+    qString = "http://dbpedia.org/resource/" + tokens[0]
+    for i in range(1, len(tokens)):
+        qString += "_" + tokens[i]
+
+    return qString
+
