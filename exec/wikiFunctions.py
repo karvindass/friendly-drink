bfb1682657a99e71e2a56d533a25d0a81312fcef
karvindass <karvin9@gmail.com>
2016-06-02 15:56:57 +0700

    WikiFunctions written

--- exec/wikiFunctions.py
+++ exec/wikiFunctions.py
@@ -3,3 +3,21 @@ import wikipedia
 import nltk
 from nltk.tokenize import word_tokenize
 
+# Recommends a page name
+def suggestPage(qString):
+    return wikipedia.search(qString)[0]
+
+# Gets RDF DBPedia Link for specific page
+def getRDFLink(pageTitle):
+    tokens = nltk.word_tokenize(pageTitle)
+    qString = "http://dbpedia.org/resource/" + tokens[0]
+    for i in range(1, len(tokens)):
+        qString += "_" + tokens[i]
+
+    return qString
+
+# Gets RDF Link from provided topic (possibly incorrect entry)
+def suggestRDFPage(topic):
+    suggestedPage = suggestPage(topic)
+    var = getRDFLink(suggestedPage)
+    return var
