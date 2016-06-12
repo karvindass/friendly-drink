444a93033f3fe6d2c11fdebc2ad3bcb07f2a830c
Sagar Mandal <sagar187@hotmail.com>
2016-06-12 00:03:40 -0400

    A state of mind variable
    Included a state of mind variable. This will be of use for the
    program’s behavior.

--- friendly-drink.py
+++ friendly-drink.py
@@ -3,6 +3,7 @@ import csv # Used for csv handling
 import nltk # Used for natural language recognition
 from nltk.stem import WordNetLemmatizer # Used to lemmatize (find root word)
 from nltk.corpus import wordnet # Wordnet for finding synonyms
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 from random import randint # Used to generate random integer
 
@@ -11,14 +12,18 @@ import ASCII_Store
 # Dictionary containing information about user
 userData = {}
 
+# Temporary storage for informaton
+tempstore= {}
+sombin= 1;
+
 # Used to reveal text with the time delay
 def reveal(text):
-    time.sleep(1)
+    time.sleep(0)
     print "FD: " + text
 
 # Used to reveal text without the time delay
 def revealInstant(text):
-    time.sleep(1)
+    time.sleep(0)
     print "FD: " + text
 
 # Used to reveal text without Time Delay and "FD: "
@@ -44,6 +49,67 @@ def getName():
     reveal("What's your name?")
     userData['userName'] = raw_input('> ') # input stored in user data dictionary
     reveal("Hey there %s, it's nice to meet you" % userData['userName'])
+    getSchool()
+#School information
+def getSchool():
+    reveal("So which college do you go to?")
+    userData['College']= raw_input('> ')
+    reveal("Holy shit! You go to %s, that's a really good place." %userData['College'])
+    howdy()
+#Getting State of Mind
+def howdy():
+    reveal("How are you?")
+    tempstore['sttofmnd']= raw_input('> ')
+    stateofmind()
+
+#Finding State of Mind
+def stateofmind():
+    usedWords = [] # Contains all the words used to make decisions on what response to make
+    tokens = word_tokenize(tempstore['sttofmnd']) # Array of sentence
+    fnchk(tokens)
+    if sombin==1:
+        reveal ("That is Amazing.")
+    else:
+        reveal ("I hope you feel better")
+
+
+def fnchk(sentence):
+    fnSynonyms= findSynonyms("fine")+ findSynonyms("good")
+    ntSynonyms= findSynonyms("not")
+    sdSynonyms= findSynonyms("sad")+findSynonyms("sick")+findSynonyms("bad")
+    for i in range (0, (len(sentence)-1)):
+        ki=0;
+        for syns in fnSynonyms:
+            if syns.lower() == sentence[i].lower():
+                ki=ki+1
+                if i==0:
+                    sombin=1
+                else:
+                    if sentence[i-1].lower()=="not":
+                        sombin=0
+                    else:
+                        for ntsyns in ntSynonyms:
+                            if sentence[i-1].lower()==ntsyns.lower():
+                                sombin=0
+                            else:
+                                sombin=1
+        if ki!=0:
+            for syns in sdSynonyms:
+                if syns.lower() == sentence[i].lower():
+                    if i==0:
+                        sombin=0
+                    else:
+                        if sentence[i-1].lower()=="not":
+                            sombin=1
+                        else:
+                            for ntsyns in ntSynonyms:
+                                if sentence[i-1].lower()==ntsyns.lower():
+                                    sombin=1
+                                else:
+                                    sombin=0
+                                return
+
+
 
 # function to print time of a location from user input using Dataset
 def getTime(locale):
